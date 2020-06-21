4284659c565da0afa9b2ebe3eac6da9feeba54a5
crashedann <22785207+sandraxchen@users.noreply.github.com>
2020-06-21 22:08:17 +1000

    initialize

---
+++ secret.R
@@ -0,0 +1,2 @@
+gmap_key <- 'AIzaSyCMVppR3b1zNeyIwcpx1y4-fwMiqG7mfSI'
+df_gsheet <- read_sheet('https://docs.google.com/spreadsheets/d/1LZs8vGkLEsZdh9b5NoY_HlNAHscy9jwAxVOJGI33dr0/edit?usp=sharing')
